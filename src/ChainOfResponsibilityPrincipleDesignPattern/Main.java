5e20646f92082f0abcd2c7ebb090d4de36aa44d6
Prathik Mishra <126965610+Prathik-mishra@users.noreply.github.com>
2024-10-18 15:28:01 +0530

    added some code in client method of chain of responsibility principle design pattern

--- src/ChainOfResponsibilityPrincipleDesignPattern/Main.java
+++ src/ChainOfResponsibilityPrincipleDesignPattern/Main.java
@@ -2,6 +2,12 @@ package ChainOfResponsibilityPrincipleDesignPattern;
 
 public class Main {
     public static void main(String[] args) {
+        LogProcessor logProcessor = new InfoLogProcessor(new DebugLogProcessor(new ErrorLogProcessor(null)));
+
+        logProcessor.log(logProcessor.ERROR, "Life me error aata he hy");
+        logProcessor.log(logProcessor.DEBUG, "Isko tho debug krna padega meri jaan");
+        logProcessor.log(logProcessor.INFO, "Li8 lo yaaro khali message hy");
+
 
     }
 }
